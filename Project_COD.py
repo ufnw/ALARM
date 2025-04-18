6284a86ffca870f3c6e974f7e9ecc00793bcedc7
Александр <osnovakasya@gmail.com>
2025-04-18 13:55:35 +0300

    Доработана функция для предпочтений пользователя. Добавлены MP3 файлы

--- Project_COD.py
+++ Project_COD.py
@@ -2,18 +2,25 @@ import datetime  # Импортируем библиотеку для работ
 import pygame   # Импортируем библиотеку для работы с окном
 
 def set_time(): # Создаём новую функцию, для установки времени и учёта предпочтений пользователя
+    sounds = {1: 'FileMP3/alarm-clock-1', 2: 'FileMP3/The-Soviet-Union-2'}
     try:
         print("\n Загрузка..." * 10)  # 10 раз пишем слово "Загрузка...", чтобы не было видно сообщение от PyGame
         time = input("Введите время для будильника в формате ЧЧ:ММ:")
+        message = input("Введите сообщение, которое нужно отправить при срабатывании будильника. Если сообщение отсутствует, укажите 0")
+        print("Сообщение,которое будет отправлено:",message)
+        number = int(input(f"Введите номер звука {sounds}"))
         time_list = [int(x) for x in time.split(":")]  # Создаём новый список, переводим все элементы из str в int
-        t = datetime.time(*time_list).strftime("%H:%M") # Уходим из списка, разделяем значения по часы:минуты. В конце обрезаем секунды
+        user_time = datetime.time(*time_list).strftime("%H:%M") # Уходим из списка, разделяем значения по часы:минуты. В конце обрезаем секунды
 
         while True:
-            if str(t) == str(datetime.datetime.now().strftime("%H:%M")):
-                print("Будильник сработал")
-                break
+            if str(user_time) == str(datetime.datetime.now().strftime("%H:%M")):
+                if int(message) == 0:
+                    print("Будильник сработал")
+                    break
+    except ValueError:
+        print(message)
     except:
-        print("\n Пожалуйста, убедитесь в правильности введенного времени")
+        print("\n Пожалуйста, убедитесь в правильности введенного Времени | Сообщение | Номера звука")
 
 #def
 
