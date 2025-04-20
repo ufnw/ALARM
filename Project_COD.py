7271722ba8e91faf6cecb279e24cc1f45ab4cab1
Александр <osnovakasya@gmail.com>
2025-04-20 13:58:37 +0300

    Изменения по мелочи

--- Project_COD.py
+++ Project_COD.py
@@ -1,21 +1,20 @@
 import datetime  # Импортируем библиотеку для работы со временем
 import pygame   # Импортируем библиотеку для работы с окном
-import sys      # Импортируем библиотеку, с помощью которой программа будет завершаться, если пользователь неправильно что-то ввёдет
 
 def set_time(): # Создаём новую функцию, для установки времени и учёта предпочтений пользователя
     sounds = {1: 'FileMP3/alarm-clock-1', 2: 'FileMP3/The-Soviet-Union-2'}
     try:
-        print("\n Загрузка..." * 10)  # 10 раз пишем слово "Загрузка...", чтобы не было видно сообщение от PyGame
-        time = input("Введите время для будильника в формате ЧЧ:ММ: ")
+
+        time = input("\nВведите время для будильника в формате ЧЧ:ММ: ")
         time_list = [int(x) for x in time.split(":")]  # Создаём новый список, переводим все элементы из str в int
         user_time = datetime.time(*time_list).strftime("%H:%M") # Уходим из списка, разделяем значения по часы:минуты. В конце обрезаем секунды
-        message = input("Введите сообщение, которое нужно отправить при срабатывании будильника. Если сообщение отсутствует, укажите 0: ")
-        number = int(input(f"Введите номер звука {sounds}: "))
+        message = input("\nВведите сообщение, которое нужно отправить при срабатывании будильника. Если сообщение отсутствует, укажите 0: ")
+        number = int(input(f"\nВведите номер звука {sounds}: "))
         return user_time, message, number
 
     except:
         print("\n Пожалуйста, убедитесь в правильности введенного Времени | Сообщение | Номера звука: ")
-        sys.exit()
+
 
 def display_sound(number):
     sounds = {1: 'FileMP3/alarm-clock-1', 2: 'FileMP3/The-Soviet-Union-2'}
@@ -40,7 +39,7 @@ def start_cycle(time,message, number_sound): # Создаём функцию, н
         while True:
             if str(time) == str(datetime.datetime.now().strftime("%H:%M")):
                 if int(message) == 0:
-                    print("\n Будильник сработал" * 10)                          # Печатаем сообщение при срабатывании будильника
+                    print("\n Пора вставать!" * 10)                          # Печатаем сообщение при срабатывании будильника
                     display_sound(number_sound)
                     break
     except ValueError:
@@ -49,12 +48,17 @@ def start_cycle(time,message, number_sound): # Создаём функцию, н
     except:
         print("Произошла какая-то ошибка...")
 
+#def
+
 
 
 def functional(): # Функция, которая будет содержать в себе остальные функции
-    time,message,number_sound = set_time()
-    start_cycle(time,message,number_sound) # Передаём значение в функцию - цикл
-
+    try:
+        print("\n Загрузка..." * 4) # 10 раз пишем слово "Загрузка...", чтобы не было видно сообщение от PyGame
+        time,message,number_sound = set_time()
+        start_cycle(time,message,number_sound) # Передаём значение в функцию - цикл
+    except:
+        functional()
 
 
 
